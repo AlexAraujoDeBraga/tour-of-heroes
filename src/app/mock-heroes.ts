f385a73ac61f43b97e5129439918869b292974d8
Alex Braga <bragadeveloper@gmail.com>
2021-07-30 09:14:51 -0300

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Jesus' },
+    { id: 2, name: 'Natanael' },
+    { id: 3, name: 'Gilza' },
+    { id: 4, name: 'Caroline' },
+    { id: 5, name: 'Álesson' }
+];
