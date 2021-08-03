dab893cfc740676837ee8a5e1eaf0300fbf21cb2
Alex Braga <bragadeveloper@gmail.com>
2021-08-03 15:48:15 -0300

    final app

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
+import { HeroService } from './../hero.service';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,16 +10,18 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
-  selectedHero?: Hero;
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-  }
+constructor(private heroService: HeroService) { }
 
-  constructor() { }
+    ngOnInit() {
+      this.getHeroes();
+    }  
 
-  ngOnInit(): void {
-  }
+    getHeroes(): void{
+      this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+    }
+
 
 }
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,7 +1,6 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-    { id: 1, name: 'Jesus' },
     { id: 2, name: 'Natanael' },
     { id: 3, name: 'Gilza' },
     { id: 4, name: 'Caroline' },
